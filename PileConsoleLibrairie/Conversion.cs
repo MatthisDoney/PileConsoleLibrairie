124a8463eae9d986c523a07ae9fd2a095b2d5e38
MatthisDoney <matthis.doney@outlook.fr>
2023-02-06 15:23:46 +0100

    Debut Programme

---
+++ PileConsoleLibrairie/Conversion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PileConsoleLibrairie
+{
+    class Conversion
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Saisie du nombre d'éléments maximum de la pile. ");
+                int nbMax = SaisirNb(1, 100);
+                Console.Write("Saisie du nombre à convertir. ");
+                int nbAConvertir = SaisirNb(2);
+                // Instanciation d'un objet de classe Pile
+            }
+        }
+    }
+}
